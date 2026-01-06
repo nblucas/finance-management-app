d8ddbb9afa2ad9c7d110e20256d4acebda92e8f8
Lucas <lucasnunesbossle@gmail.com>
2026-01-06 14:33:04 -0300

    feat: redoing main interface, so some components are being deleted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
 import "./global-styles.css"
 import Header from "./layouts/header/Header"
-import Tab from "./components/tab/Tab"
-import { entitiesTabValues } from "./models/model-entities"
-import CategoryRegistrantion from "./pages/CategoryRegistration"
 
 function App() {
   return (
     <>
       <Header />
-      <Tab tabTitles={entitiesTabValues}/>
-      <CategoryRegistrantion />
     </>
   )
 }
