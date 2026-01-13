ee34c5a9d94cf96643003dba0d3242dfabd39959
Lucas <lucasnunesbossle@gmail.com>
2026-01-13 18:26:54 -0300

    feat: handleClick prop added to button component

--- frontend/src/design-system/button/Button.tsx
+++ frontend/src/design-system/button/Button.tsx
@@ -4,15 +4,19 @@ type ButtonProps = {
   label: string
   size: "small" | "medium" | "large" | "adjust"
   state: "normal" | "disabled" | "loading"
+  handleClick: () => void
 }
 
 export function Button(props: Readonly<ButtonProps>) {
-  const { label, size, state } = props
+  const { label, size, state, handleClick } = props
 
   const isLoading = state === "loading"
 
   return (
-    <button className={`${styles.button} ${styles[size]} ${styles[state]}`}>
+    <button
+      onClick={handleClick}
+      className={`${styles.button} ${styles[size]} ${styles[state]}`}
+    >
       {isLoading ? "Loading..." : label}
     </button>
   )
