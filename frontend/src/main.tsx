7deaaafa09ca357ca7f93910a2c800c2bbb9c168
Lucas <lucasnunesbossle@gmail.com>
2026-01-14 13:57:38 -0300

    fix: removing older routes in create router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import "./global-styles.css"
-import ExpensesPage from './pages/expenses-page/ExpensesPage.tsx'
-import NewExpensePage from './pages/new-expense-page/NewExpensePage.tsx'
 import TestPage from './pages/test-page/TestPage.tsx'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: ExpensesPage,
-  }, 
-  {
-    path: "/register-expenses",
-    Component: NewExpensePage
-  },
   {
     path: "/test",
     Component: TestPage
