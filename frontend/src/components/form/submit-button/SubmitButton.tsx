d97263b5c5eb03908633050b91879e0d8281c593
Lucas <lucasnunesbossle@gmail.com>
2026-01-14 13:05:47 -0300

    refactor: removing all old components

--- frontend/src/components/form/submit-button/SubmitButton.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./SubmitButton.module.css"
-
-export default function SubmitButton() {
-  return (
-    <input type="submit" value="Submit" className={styles.button} />
-  )
-}
--- frontend/src/components/table/search-bar/SearchBar.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./SearchBar.module.css"
-
-export default function SearchBar() {
-  return (
-    <div>
-      <input type="text" placeholder="Search" className={styles.input}/>
-    </div>
-  )
-}
