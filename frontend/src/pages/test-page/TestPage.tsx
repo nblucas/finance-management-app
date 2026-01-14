d9d12247f6e93b84c8a8662e616b77700867e718
Lucas <lucasnunesbossle@gmail.com>
2026-01-14 11:37:43 -0300

    feat: most recent test page tests

--- frontend/src/pages/test-page/TestPage.tsx
+++ frontend/src/pages/test-page/TestPage.tsx
@@ -1,27 +1,53 @@
 import styles from "./TestPage.module.css"
-import { Modal } from "../../design-system/modal"
-import { useState } from "react"
-import { Button } from "../../design-system/button"
+import { SideNav } from "../../design-system/side-nav"
+import { Link } from "../../design-system/link"
+
+const listData = [
+  {
+    id: 0,
+    iconSrc: "./src/assets/icons/filter_icon.png",
+    iconAltText: "Icon that represents the action of filtering data.",
+    label: "Dashboard",
+    to: "/test"
+  },
+  {
+    id: 1,
+    iconSrc: "./src/assets/icons/filter_icon.png",
+    iconAltText: "Icon that represents the action of filtering data.",
+    label: "Dashboard",
+    to: "/test"
+  },
+  {
+    id: 2,
+    iconSrc: "./src/assets/icons/filter_icon.png",
+    iconAltText: "Icon that represents the action of filtering data.",
+    label: "Dashboard",
+    to: "/test"
+  },
+  {
+    id: 3,
+    iconSrc: "./src/assets/icons/filter_icon.png",
+    iconAltText: "Icon that represents the action of filtering data.",
+    label: "Dashboard",
+    to: "/test"
+  },
+  {
+    id: 4,
+    iconSrc: "./src/assets/icons/filter_icon.png",
+    iconAltText: "Icon that represents the action of filtering data.",
+    label: "Dashboard",
+    to: "/test"
+  }
+]
 
 export default function TestPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false) 
 
   return (
     <div className={styles.container}>
-      <Button
-        label="Open modal"
-        handleClick={() => setIsModalOpen(true)}
-        size="small"
-        state="normal"
-      />
-      <Modal
-        body={() => <div>Hello Modal</div>}
-        size="medium"
-        title="Modal title check it out"
-        isOpen={isModalOpen}
-        handleClose={() => setIsModalOpen(false)}
-        handleCancel={() => setIsModalOpen(false)}
-        handleConfirm={() => setIsModalOpen(false)}
+      <SideNav
+        logoSrc="./src/assets/icons/logoipsum.png"
+        logoAltText="logo"
+        listItemsData={listData}
       />
     </div>
   )
