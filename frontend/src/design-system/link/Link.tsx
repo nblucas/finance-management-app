e2dacc6bbd9ba5b63ee410548d834496cfc7096e
Lucas <lucasnunesbossle@gmail.com>
2026-01-14 11:34:55 -0300

    fix: link component updated

--- frontend/src/design-system/link/Link.tsx
+++ frontend/src/design-system/link/Link.tsx
@@ -5,15 +5,16 @@ type LinkProps = {
   iconSrc?: string,
   iconAltText?: string,
   label: string,
-  size: "small" | "medium" | "large" | "adjust" 
+  size: "small" | "medium" | "large" | "adjust"
+  contentPosition: "left" | "center"
 }
 
 export function Link(props: Readonly<LinkProps>) {
-  const { to, iconSrc, iconAltText, label, size } = props 
+  const { to, iconSrc, iconAltText, label, size, contentPosition } = props 
 
   return (
-    <a href={to} className={`${styles.link} ${styles[size]}`}>
-      {iconSrc && <img src={iconSrc} alt={iconAltText} />}
+    <a href={to} className={`${styles.link} ${styles[size]} ${styles[contentPosition]}`}>
+      {iconSrc && <img src={iconSrc} alt={iconAltText} className={styles.icon} />}
       <p>{label}</p>
     </a>
   )
